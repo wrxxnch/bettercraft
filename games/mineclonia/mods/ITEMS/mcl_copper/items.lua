f299a9c221075c12253b0c2397ae237dc01e7da4
root <root@Sheila.localdomain>
2026-01-31 21:11:09 -0300

    added copper nugget

--- games/mineclonia/mods/ITEMS/mcl_copper/items.lua
+++ games/mineclonia/mods/ITEMS/mcl_copper/items.lua
@@ -2,15 +2,24 @@ local S = core.get_translator("mcl_copper")
 
 core.register_craftitem("mcl_copper:copper_ingot", {
 	description = S("Copper Ingot"),
-	_doc_items_longdesc = S("Molten Raw Copper. It is used to craft blocks."),
+	_doc_items_longdesc = S("Molten Copper. It is used to craft armor, tools, and blocks."),
 	inventory_image = "mcl_copper_ingot.png",
 	groups = { craftitem = 1 },
 	_mcl_armor_trim_color = "#c36447",
 	_mcl_armor_trim_desc = S("Copper Material"),
 	_mcl_crafting_output = {
 		line_tall3 = {output = "mcl_lightning_rods:rod"},
-		square3 = {output = "mcl_copper:block"}
+		square3 = {output = "mcl_copper:block"},
+		single = {output = "mcl_copper:copper_nugget 9"}
 	}
+})
+
+core.register_craftitem("mcl_copper:copper_nugget", {
+	description = S("Copper Nugget"),
+	_doc_items_longdesc = S("Copper nuggets are very small pieces of molten copper; the main purpose is to create copper ingots."),
+	inventory_image = "mcl_copper_nugget.png",
+	groups = { craftitem = 1 },
+	_mcl_crafting_output = {square3 = {output = "mcl_copper:copper_ingot"}}
 })
 
 core.register_craftitem("mcl_copper:raw_copper", {
